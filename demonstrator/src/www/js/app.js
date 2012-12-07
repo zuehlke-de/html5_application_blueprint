27a9507836e905db92e86e0562bdd0c9c3e5759e
Sebastian Büttner <seb_bue@web.de>
2012-12-07 13:40:24 +0100

    Latest Changes

--- demonstrator/src/www/js/app.js
+++ demonstrator/src/www/js/app.js
@@ -3,6 +3,29 @@ require([
     'log/LogCollection'
 ], function (LogView, LogCollection) {
 
+
+    var POST_API = '/api/v1/post/'
+
+    window.Post = Backbone.Model.extend({
+        url: POST_API
+    })
+
+    window.Posts = Backbone.Collection.extend({
+        model: Post,
+        url: POST_API,
+
+        get_coordinates: function() {
+            coords = _.map(this.models, function(post) {
+                lat = post.attributes.lat;
+                lng = post.attributes.lng;
+                return {lat:lat, lng:lng}
+            });
+            return coords
+        }
+    })
+
+    window.Map = Backbone.Model.extend({})
+
     /*
      * Views
      */
@@ -15,13 +38,32 @@ require([
         }
     });
 
-
     var MapView = Backbone.View.extend({
 
-        tagName : "div",
+        initialize: function(opts) {
+            _.bindAll(this, 'render');
+            this.posts = opts.posts;
+            this.posts.add()
+            this.posts.on('change', this.render);
+            this.render();
+        },
 
-        render : function () {
-            $(this.el).html("Dies ist die MapView");
+        render: function() {
+            var loc = new google.maps.LatLng('-34.397','150.644');
+            var options = {
+                zoom: 6,
+                center: loc,
+                mapTypeId: google.maps.MapTypeId.ROADMAP
+            };
+            // create map
+            this.map = new google.maps.Map(this.el, options);
+
+            // add a marker
+            var marker = new google.maps.Marker({
+                position: loc,
+                map :this.map
+            });
+
             return this;
         }
     });
@@ -55,12 +97,29 @@ require([
 
             this.headerView = new HeaderView();
             this.logView = new LogView({collection: this.logCollection});
-            this.mapView = new MapView();
+
+            post = new Post();
+
+            posts = new Posts();
+
+            map = new Map({
+                latitude: '-34.397',
+                longitude: '150.644'
+            });
+
+            this.mapView = new MapView({
+                el: $('#map'),
+                model: map,
+                posts: posts
+            });
+
+            this.mapView.render();
+
             this.timelineView = new TimelineView();
 
             $('#header').html(this.headerView.render().el);
             $('#log').html(this.logView.render().el);
-            $('#map').html(this.mapView.render().el);
+            //$('#map').html(this.mapView.render().el);
             $('#timeline').html(this.timelineView.render().el);
         }
     });
